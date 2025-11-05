170ead957356971b412fc188dc9c242e55882819
Sarah Nicole Jallorina <rahneejall@gmail.com>
2025-11-05 18:06:56 +0800

    Update FloodData.java
    added no argument constructor

--- FloodData.java
+++ FloodData.java
@@ -2,26 +2,28 @@ public class FloodData
 {
     private int flood_id;
     private String flood_factor;
-    private int location_id;
-    private String alt_delivery_method;
-    private int affected_households;
     private float avg_water_level;
+    private int affected_households;
     private String road_condition;
     private boolean special_packaging;
+    private String alt_delivery_method;
+    private int location_id;
 
-    public FloodData(int flood_id, String flood_factor, int location_id, String alt_delivery_method, 
-                     int affected_households, float avg_water_level, String road_condition, 
-                     boolean special_packaging)
+    public FloodData(int flood_id, String flood_factor, float avg_water_level, 
+                     int affected_households, String road_condition, boolean special_packaging,
+                     String alt_delivery_method, int location_id)
                      {
                         this.flood_id = flood_id;
                         this.flood_factor = flood_factor;
-                        this.location_id = location_id;
-                        this.alt_delivery_method = alt_delivery_method;
-                        this.affected_households = affected_households;
                         this.avg_water_level = avg_water_level;
+                        this.affected_households = affected_households;
                         this.road_condition = road_condition;
                         this.special_packaging = special_packaging;
+                        this.alt_delivery_method = alt_delivery_method;
+                        this.location_id = location_id;
                      }
+
+    public FloodData() {}
 
     public int getFloodID()
     {
